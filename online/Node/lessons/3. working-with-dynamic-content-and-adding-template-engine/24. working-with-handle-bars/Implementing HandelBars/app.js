d158f90983bf1b8d0ee981e062aa7761cd54d545
navneeet94 <98764448+navneeet94@users.noreply.github.com>
2022-10-03 10:47:37 +0530

    update code

---
+++ online/Node/lessons/3. working-with-dynamic-content-and-adding-template-engine/24. working-with-handle-bars/Implementing HandelBars/app.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin',adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=> {
+    res.status(404).render('not-found', {docTitle:'Page Not Found'})
+})
+
+app.listen(3000)
