933aa31353f6a7134ffdbf1e6bd99bdfae7accb2
navneeet94 <98764448+navneeet94@users.noreply.github.com>
2022-10-16 15:34:50 +0530

    update code / working with handelbars

---
+++ online/Node/lessons/3. working-with-dynamic-content-and-adding-template-engine/24. working-with-handlebars/adding a layout/routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    products = adminData.products
+    res.render('shop' , {prods:products, docTitle: 'Shop'});
+});
+
+
+
+module.exports = router
